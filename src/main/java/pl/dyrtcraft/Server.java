9888d5942495bf7513996cf1274a127917c6c7ce
TheMolkaPL <themolkapl@gmail.com>
2013-12-08 21:22:41 +0100

    Dokumentacja dla Server

--- src/main/java/pl/dyrtcraft/Server.java
+++ src/main/java/pl/dyrtcraft/Server.java
@@ -1,5 +1,14 @@
 package pl.dyrtcraft;
 
+/**
+ * {@link Enum} z serwerami DyrtCraft Network
+ * @author TheMolkaPL
+ * @since 1.0
+ * @see BungeeCord#getServer(String)
+ * @see BungeeCord#getServerAddress(Server)
+ * @see BungeeCord#getServerName(Server)
+ * @see Member#connect(Server)
+ */
 public enum Server {
 	
 	APOCALYPTO_HARDCORE, LOBBY, MINEZ, PAINTBALL, PVP, SKYBLOCK
